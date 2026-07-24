66149aef2de3f84e584983ed32445456c374f7ad
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-07-24 18:51:14 -0300

    fix: update JWT filter logic to support token refresh

--- src/main/java/com/douglaasph/clinic_api/config/security/JwtFilter.java
+++ src/main/java/com/douglaasph/clinic_api/config/security/JwtFilter.java
@@ -1,13 +1,18 @@
 package com.douglaasph.clinic_api.config.security;
 
+import com.douglaasph.clinic_api.exceptions.TokenException;
 import com.douglaasph.clinic_api.services.JWTService;
 import com.douglaasph.clinic_api.services.AuthService;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -30,7 +35,8 @@ public class JwtFilter extends OncePerRequestFilter {
         String requestURI = request.getRequestURI();
 
         // If it is the patient login or registration route, let it pass through WITHOUT validating the JWT.
-        if ("/auth/login".equals(requestURI) || "/patient/register".equals(requestURI)) {
+        System.out.println(requestURI);
+        if ("/auth/login".equals(requestURI) || "/patient/register".equals(requestURI) || requestURI.startsWith("/refresh-token/")) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -39,19 +45,35 @@ public class JwtFilter extends OncePerRequestFilter {
         String token = null;
         String email = null;
 
-        if(authHeader != null && authHeader.startsWith("Bearer ")) {
-            token = authHeader.substring(7);
-            email =jwtService.extractUserName(token);
-        }
-
-        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = context.getBean(AuthService.class).loadUserByUsername(email);
-            if (jwtService.validateToken(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+        try {
+            if(authHeader != null && authHeader.startsWith("Bearer ")) {
+                token = authHeader.substring(7);
+                email =jwtService.extractUserName(token);
             }
+
+            if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = context.getBean(AuthService.class).loadUserByUsername(email);
+                if (jwtService.validateToken(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
+            }
+
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            handleUnauthorizedError(response, "The JWT access token has expired.");
+        } catch (JwtException e) {
+            handleUnauthorizedError(response, "The JWT access token is invalid.");
         }
-        filterChain.doFilter(request, response);
+    }
+
+    private void handleUnauthorizedError(HttpServletResponse response, String message) throws IOException {
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding("UTF-8");
+
+        String jsonResponse = String.format("{\"status\": 401, \"error\": \"Unauthorized\", \"message\": \"%s\"}", message);
+        response.getWriter().write(jsonResponse);
     }
 }
