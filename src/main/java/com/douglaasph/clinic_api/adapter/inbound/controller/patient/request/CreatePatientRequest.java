61adf3bee8bc2ba4184dd9ca6d84a5376ef88de4
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-08-08 03:37:24 -0300

    fix:resolve errors dto

--- src/main/java/com/douglaasph/clinic_api/adapter/inbound/controller/patient/request/CreatePatientRequest.java
+++ src/main/java/com/douglaasph/clinic_api/adapter/inbound/controller/patient/request/CreatePatientRequest.java
@@ -16,11 +16,11 @@ public class CreatePatientRequest {
     private String password;
 
     @NotBlank(message = "The cpf cannot be blank.")
-    @Size(max = 11, min = 11, message = "CPF must contain 11 digits. Example: 00000000000")
+    @Size(max = 14, min = 14, message = "CPF must contain 11 digits. Example: 000.000.000-00")
     private String cpf;
 
     @NotBlank(message = "The phone cannot be blank.")
-    @Size(max = 13, min = 13, message = "Phone must contain 13 digits. Example: 5581900000000")
+    @Size(max = 15, min = 15, message = "Phone must contain 13 digits. Example: (81) 90000-0000")
     private String phone;
     
     public CreatePatientRequest() {}
